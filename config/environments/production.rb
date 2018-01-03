3abc756a3daced9487c96713fc36f40f1362c49b
giangmt <maitruonggiangbkhn@gmail.com>
2018-01-03 20:43:04 +0700

    Update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  host = 'boiling-coast-46225.herokuapp.com'     # Cloud IDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
