cd29595e5611f4413d0cdd20888674aa15801ec9
giangmt <maitruonggiangbkhn@gmail.com>
2018-01-05 19:35:19 +0700

    Add mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Gmail configuration
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
