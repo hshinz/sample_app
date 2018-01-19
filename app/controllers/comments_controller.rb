3d0dda9ac3d8c88532f7e15b3b12c160c423fea3
giangmt <maitruonggiangbkhn@gmail.com>
2018-01-19 21:11:10 +0700

    Add Edit, Destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
-	before_action :load_comment, only: [:edit, :update]
+	before_action :logged_in_user
+	before_action :load_comment, only: [:edit, :update, :destroy]
+	before_action :load_post
+
 	def create
 		@comment = Comment.new comment_params
 		if @comment.save
@@ -26,11 +29,40 @@ class CommentsController < ApplicationController
 
 	def update
 		if @comment.update_attributes comment_params
-			flash[:success] = "Update comment successfully"
-			redirect_to @comment.post
+			# flash[:success] = "Update comment successfully"
+			# redirect_to @comment.post
+			respond_to do |format|
+				format.html 
+				format.js
+				format.json
+			end
 		else
-			flash.now[:danger] = "Update comment failed"		
-			render :edit
+			# flash.now[:danger] = "Update comment failed"		
+			# render :edit
+			respond_to do |format|
+				format.html 
+				format.js
+				format.json
+			end
+		end
+	end
+
+	def destroy
+		# Comment.find(params[:id]).destroy
+		# flash[:success] = "Comment deleted"
+		# redirect_to @post
+		if @comment.destroy
+			respond_to do |format|
+				format.html 
+				format.js
+				format.json
+			end
+		else
+			respond_to do |format|
+				format.html 
+				format.js
+				format.json
+			end
 		end
 	end
 
@@ -40,8 +72,15 @@ class CommentsController < ApplicationController
 	end
 
 	def load_comment
-		@comment = Comment.find_by id: params[:id]
+		@comment = current_user.comments.find_by id: params[:id]
+		return if @comment
+		flash.now[:danger] = "Access denied"
 		redirect_to root_path unless @comment
+	end
+
+	def load_post
+		@post = Post.find_by id: params[:post_id]
+		redirect_to root_path unless @post
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :posts
-  resources :comments, only: [:create, :edit, :update]
+  resources :posts do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
 end
